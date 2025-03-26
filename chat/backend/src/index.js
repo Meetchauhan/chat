70ae12b06cc62ce24df5140830814047182eaa5d
hetarthee <hetarthee@binaryic.co.in>
2025-03-26 14:10:49 +0530

    static file

--- chat/backend/src/index.js
+++ chat/backend/src/index.js
@@ -9,7 +9,9 @@ import registerRoute from "./routes/user.routes.js";
 import messageRoute from "./routes/message.routes.js";
 import connecUsersRoute from "./routes/connectingUsers.routes.js";
 import { app, server } from "./util/socket.js";
-
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // const app = express();
 // const server = createServer(app);
 
@@ -57,6 +59,14 @@ app.get("/", (req, res) => {
 
 // ✅ Fix: Listen on `server`, NOT `app`
 const PORT = 1322;
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist"))); // Adjust path if needed
+
+// Fallback route for React
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html")); // Adjust path
+  connectDB();
+});
 server.listen(PORT, () => {
   console.log(`🚀 Server is running on PORT: ${PORT}`);
   connectDB();
