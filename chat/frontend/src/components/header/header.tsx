c8ee8439911942b5924a6e02fd0b7eacf6872db3
hetarthee <hetarthee@binaryic.co.in>
2025-03-27 19:12:08 +0530

    getRequest

--- chat/frontend/src/components/header/header.tsx
+++ chat/frontend/src/components/header/header.tsx
@@ -38,6 +38,7 @@ const Header = () => {
 
   const handleOpenMenu = () => {
     dispatch(openMenu());
+    dispatch(getRequest())
   };
 
   const isMenuOpen = useSelector((item: RootState) => item?.menu?.value);
