7c66adc84e59f386d9daf3c7199c46216d0e1010
hetarthee <hetarthee@binaryic.co.in>
2025-04-04 11:42:54 +0530

    ios config

--- chat/frontend/src/firebase.ts
+++ chat/frontend/src/firebase.ts
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import {
+  getMessaging,
+  getToken,
+  isSupported,
+  onMessage,
+} from "firebase/messaging";
+import { isIOS } from "./utils/isIos";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,43 +14,27 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 // const VAPID_KEY = import.meta.env.VITE_VAPID_KEY;
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const messaging = getMessaging(app);
-
-// ✅ Register Service Worker Correctly
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/firebase-messaging-sw.js") // Must be inside the `public` folder
-    .then((registration) => {
-      console.log("✅ Service Worker registered:", registration);
-    })
-    .catch((error) => {
-      console.error("❌ Service Worker registration failed:", error);
-    });
+if (!isIOS() && (await isSupported())) {
+  // ✅ Register Service Worker Correctly
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js") // Must be inside the `public` folder
+      .then((registration) => {
+        console.log("✅ Service Worker registered:", registration);
+      })
+      .catch((error) => {
+        console.error("❌ Service Worker registration failed:", error);
+      });
+  }
+} else {
+  console.log("Push notifications skipped on iOS or unsupported browser.");
 }
-
-// ✅ Function to Request Notification Permission
-// export const requestNotificationPermission = async () => {
-//   const permission = await Notification.requestPermission();
-//   if (permission !== "granted") {
-//     console.warn("❌ Notification permission denied.");
-//     return;
-//   }
-
-//   try {
-//     const token = await getToken(messaging, {
-//       vapidKey: VAPID_KEY.trim(), // Ensure no spaces
-//     });
-
-//     console.log("✅ FCM Token:", token);
-//     return token;
-//   } catch (error) {
-//     console.error("❌ Error getting FCM token:", error);
-//   }
-// };
 
 export { messaging, getToken, onMessage };
---
+++ chat/frontend/src/utils/isIos.tsx
@@ -0,0 +1,10 @@
+declare global {
+    interface Window {
+      MSStream?: unknown;
+    }
+  }
+  
+  export const isIOS = (): boolean => {
+    const userAgent = window.navigator.userAgent;
+    return /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+  };
