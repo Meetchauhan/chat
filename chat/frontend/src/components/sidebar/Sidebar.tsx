bf03f02857d43f674faea5ba2d5f7c54515300f4
hetarthee <hetarthee@binaryic.co.in>
2025-04-16 11:29:33 +0530

    sidebar

--- chat/frontend/src/components/sidebar/Sidebar.tsx
+++ chat/frontend/src/components/sidebar/Sidebar.tsx
@@ -41,26 +41,26 @@ const Sidebar = () => {
         </div>
       </div>
     );
-  }
-  return (
-    <div className="bg-gray-900 w-full md:w-[35%] lg:w-[25%] min-h-full h-[calc(100dvh-53px)] md:h-[calc(100dvh-73px)] overflow-x-auto">
-      {users?.length > 0 ? (
-        <div className="">
-          <UsersList />
-        </div>
-      ) : (
-        <div className="text-orange-600 h-full flex justify-center items-center flex-col">
-          <p>No Users to chat </p>
-          <p>To get Users to chat click here</p>
-          <Link
-            to="/find-users"
-            className="bg-white px-3 py-1 rounded-sm text-[15px] mt-2"
-          >
-            Find Users
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+  } else
+    return (
+      <div className="bg-gray-900 w-full md:w-[35%] lg:w-[25%] min-h-full h-[calc(100dvh-53px)] md:h-[calc(100dvh-73px)] overflow-x-auto">
+        {users?.length > 0 ? (
+          <div className="">
+            <UsersList />
+          </div>
+        ) : (
+          <div className="text-orange-600 h-full flex justify-center items-center flex-col">
+            <p>No Users to chat </p>
+            <p>To get Users to chat click here</p>
+            <Link
+              to="/find-users"
+              className="bg-white px-3 py-1 rounded-sm text-[15px] mt-2"
+            >
+              Find Users
+            </Link>
+          </div>
+        )}
+      </div>
+    );
 };
 export default Sidebar;
