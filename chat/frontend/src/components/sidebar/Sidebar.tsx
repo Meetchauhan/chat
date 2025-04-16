4130b32f3410d7568190731ef2bdce87b2e8a40d
hetarthee <hetarthee@binaryic.co.in>
2025-04-16 17:43:23 +0530

    fix side bar

--- chat/frontend/src/components/sidebar/Sidebar.tsx
+++ chat/frontend/src/components/sidebar/Sidebar.tsx
@@ -4,63 +4,34 @@ import { AppDispatch } from "../../store/store";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { getUsers } from "../../features/ChatSlice";
-import {
-  useChatUserLoading,
-  useGetChatUsers,
-} from "../../customHooks/useGetChatUsers";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useGetChatUsers } from "../../customHooks/useGetChatUsers";
 
 const Sidebar = () => {
   const dispatch = useDispatch<AppDispatch>();
   const users = useGetChatUsers();
-  const userLoading = useChatUserLoading();
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
-  // if (userLoading) {
-  //   return (
-  //     <div className="bg-gray-900 w-full md:w-[35%] lg:w-[25%] min-h-full h-[calc(100dvh-53px)] md:h-[calc(100dvh-73px)] overflow-x-auto">
-  //       <div className="text-orange-600 h-full flex justify-center items-center flex-col">
-  //         <SkeletonTheme baseColor="#39424e" highlightColor="#f54a00">
-  //           <div className="my-2">
-  //             <Skeleton height={60} duration={0.9} />
-  //           </div>
-  //           <div className="my-2">
-  //             <Skeleton height={60} duration={0.9} />
-  //           </div>
-  //           <div className="my-2">
-  //             <Skeleton height={60} duration={0.9} />
-  //           </div>
-  //           <div className="my-2">
-  //             <Skeleton height={60} duration={0.9} />
-  //           </div>
-  //           <div className="my-2">
-  //             <Skeleton height={60} duration={0.9} />
-  //           </div>
-  //         </SkeletonTheme>
-  //       </div>
-  //     </div>
-  //   );
-  // } else
-    return (
-      <div className="bg-gray-900 w-full md:w-[35%] lg:w-[25%] min-h-full h-[calc(100dvh-53px)] md:h-[calc(100dvh-73px)] overflow-x-auto">
-        {users?.length > 0 ? (
-          <div className="">
-            <UsersList />
-          </div>
-        ) : (
-          <div className="text-orange-600 h-full flex justify-center items-center flex-col">
-            <p>No Users to chat </p>
-            <p>To get Users to chat click here</p>
-            <Link
-              to="/find-users"
-              className="bg-white px-3 py-1 rounded-sm text-[15px] mt-2"
-            >
-              Find Users
-            </Link>
-          </div>
-        )}
-      </div>
-    );
+
+  return (
+    <div className="bg-gray-900 w-full md:w-[35%] lg:w-[25%] min-h-full h-[calc(100dvh-53px)] md:h-[calc(100dvh-73px)] overflow-x-auto">
+      {users?.length > 0 ? (
+        <div className="">
+          <UsersList />
+        </div>
+      ) : (
+        <div className="text-orange-600 h-full flex justify-center items-center flex-col">
+          <p>No Users to chat </p>
+          <p>To get Users to chat click here</p>
+          <Link
+            to="/find-users"
+            className="bg-white px-3 py-1 rounded-sm text-[15px] mt-2"
+          >
+            Find Users
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 export default Sidebar;
